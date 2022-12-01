bc0bdec597d2938905be57d55ebcb53e55080dfe
Abdullah <abdullahmajeed969@gmail.com>
2022-12-02 01:54:58 +0500

    model tflite working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 import 'package:image_picker/image_picker.dart';
@@ -29,26 +32,20 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
-  Future<void> getImage() async{
-
-    Directory tempDir = await getTemporaryDirectory();
-    String tempPath = tempDir.path;
-    //File image = File('$tempPath/assets/bus.jpg');
-
-    print(tempPath);
-
-    // ImageProcessor imageProcessor = ImageProcessorBuilder()
-    //     .add(ResizeOp(224, 224, ResizeMethod.NEAREST_NEIGHBOUR))
-    //     .build();
-    //
-    // TensorImage tensorImage = TensorImage.fromFile(File(image!.path));
-    // print(tensorImage);
-
-    //inputImage = imageProcessor.process(inputImage);
+  Future<TensorImage> getImage(Interpreter interpreter,TensorImage inputImage) async{
+    var _inputShape = interpreter.getInputTensor(0).shape;
+    var _outputShape = interpreter.getOutputTensor(0).shape;
+    var _outputType = interpreter.getOutputTensor(0).type;
+    ImageProcessor imageProcessor = ImageProcessorBuilder()
+        .add(ResizeOp(
+        _inputShape[1], _inputShape[2], ResizeMethod.NEAREST_NEIGHBOUR))
+        .build();
+    inputImage = imageProcessor.process(inputImage);
+return inputImage;
   }
 
   Future<void> loadModel() async {
-    final interpreter = await Interpreter.fromAsset('tflite-416-fp32.tflite');
+    final interpreter = await Interpreter.fromAsset('model.tflite');
     var outputTensors=(interpreter.getOutputTensors());
     var _outputShapes = [];
     var _outputTypes = [];
@@ -57,8 +54,66 @@ class MyHomePage extends StatelessWidget {
       _outputTypes.add(element.type);
     });
     print('Output Shapes $_outputShapes');
-    print('Output types $_outputTypes');
-    getImage();
+    // print('Output types $_outputTypes');
+    final byteData = await rootBundle.load("assets/salad.jpg");
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
+    File tempImage = File("${tempDir.path}/assets/salad.jpg")
+      ..createSync(recursive: true)
+      ..writeAsBytesSync(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+    TensorImage inputImage = TensorImage.fromFile(tempImage);
+
+   inputImage = await getImage(interpreter,inputImage);
+      print(inputImage);
+    // TensorBuffers for output tensors
+    // TensorBuffer outputLocations = TensorBufferFloat(_outputShapes[0]);
+    // TensorBuffer outputClasses = TensorBufferFloat(_outputShapes[1]);
+    // TensorBuffer outputScores = TensorBufferFloat(_outputShapes[2]);
+    // TensorBuffer numLocations = TensorBufferFloat(_outputShapes[3]);
+    List<Object> inputs = [inputImage.buffer];
+    // Outputs map
+    // Map<int, Object> outputs = {
+    //   0: outputLocations.buffer,
+    //   1: outputClasses.buffer,
+    //   2: outputScores.buffer,
+    //   3: numLocations.buffer,
+    // };
+    //second way....
+    TensorBuffer output0 = TensorBuffer.createFixedSize(
+        interpreter.getOutputTensor(0).shape,
+        interpreter.getOutputTensor(0).type);
+    TensorBuffer output1 = TensorBuffer.createFixedSize(
+        interpreter.getOutputTensor(1).shape,
+        interpreter.getOutputTensor(1).type);
+
+    Map<int, ByteBuffer> outputs = {0: output0.buffer, 1: output1.buffer};
+
+
+
+    // int NUM_RESULTS = 10;
+
+   interpreter.runForMultipleInputs(inputs, outputs);
+    // print(outputs);
+    // // Maximum number of results to show
+    // int resultsCount = min(NUM_RESULTS, numLocations.getIntValue(0));
+    // print('resultsCount:$resultsCount');
+    // // Using labelOffset = 1 as ??? at index 0
+    // int labelOffset = 1;
+    // int INPUT_SIZE = 300;
+    // // Using bounding box utils for easy conversion of tensorbuffer to List<Rect>
+    // List<Rect> locations = BoundingBoxUtils.convert(
+    //   tensor: outputLocations,
+    //   valueIndex: [1, 0, 3, 2],
+    //   boundingBoxAxis: 2,
+    //   boundingBoxType: BoundingBoxType.BOUNDARIES,
+    //   coordinateType: CoordinateType.RATIO,
+    //   height: INPUT_SIZE,
+    //   width: INPUT_SIZE,
+    // );
+    // print('location:$locations');
+
   }
     @override
     Widget build(BuildContext context) {
